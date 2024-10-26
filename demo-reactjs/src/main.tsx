65a7fe9912805655105a229ec2e0dc5974505f61
DanielSolano8 <102555742+DanielSolano8@users.noreply.github.com>
2024-10-25 19:32:03 -0600

    Laboratorio 9 React

--- demo-reactjs/src/main.tsx
+++ demo-reactjs/src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import RegisterUser from './pages/RegisterUser/index.tsx';
+import Login from './pages/Login/Login.tsx';
 //import './index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RegisterUser />
+		<Login />
 	</StrictMode>,
 );
--- demo-reactjs/src/pages/RegisterUser/index.tsx
+++ demo-reactjs/src/pages/RegisterUser/index.tsx
@@ -30,4 +30,3 @@ const RegisterUser = () => {
 	);
 };
 export default RegisterUser;
-
